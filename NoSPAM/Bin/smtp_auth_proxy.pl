9ccdb033b983a4d20d0e8590a9095720bf426da1
Zhuohuan LI <zixia@zixia.net>
2004-02-19 20:04:09 +0000

    a simple perl copy of vchkpw

---
+++ NoSPAM/Bin/smtp_auth_proxy.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+use Net::SMTP_auth;
+
+my $REMOTE_SMTP = '211.151.91.4';
+
+my ($user, $pass, $challenge) = &get_info();
+
+$smtp = Net::SMTP_auth->new($REMOTE_SMTP);
+
+open ( WFD, ">>/var/log/chkpw.log" );
+
+if ( $smtp->auth('LOGIN', $user, $pass) ){
+	print WFD "$user, $pass, $challenge auth with $REMOTE_SMTP succ.\n";
+        exit 0;
+}else{
+	print WFD "$user, $pass, $challenge auth with $REMOTE_SMTP failed.\n";
+}
+close ( WFD );
+
+exit 20;
+
+sub get_info
+{
+	my ($user, $pass, $challenge);
+	my $buf = ' ' x 1024;
+
+	open ( FD, "<&3" ) or die "only talk with qmail-smtpd!";
+	$n = read ( FD, $buf, 1024 );
+	close ( FD );
+
+	if ( $buf =~ /\0/ ){
+		($user,$pass,$challenge) = split ( /\0/, $buf );
+	}else{
+		open ( WFD, ">>/var/log/chkpw.log" );
+		print WFD "error: $n, $buf\n";
+		close ( WFD );
+	}
+
+	($user,$pass,$challenge);
+}
