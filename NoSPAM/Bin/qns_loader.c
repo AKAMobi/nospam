ae2f56b04a747307a3128ea0cfc47435dae67489
Zhuohuan LI <zixia@zixia.net>
2004-02-23 04:54:02 +0000

    compile perl script can't use setuidperl, so add a c prgram to setuid, and "load" it.

---
+++ NoSPAM/Bin/qns_loader.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define QNS_BINARY "/var/qmail/bin/qmail-ns-queue"
+
+int main( int argc, char* argv[] )
+{
+	//execvp(argv[1], argv+1);
+	strcpy ( argv[0], QNS_BINARY );
+
+	setuid ( 0 );
+	setgid ( 0 );
+
+	seteuid ( 0 );
+	setegid ( 0 );
+
+	execvp( QNS_BINARY, argv );
+
+	fprintf ( stderr, "443 qns_loader error\n" );
+}
+
