918857b82fafd3f4a7bb11e83776308059c35da1
Zhuohuan LI <zixia@zixia.net>
2004-02-23 15:26:00 +0000

    auto add rcpthost when add smtproutes

--- mailadm/controlSmtproute.php
+++ mailadm/controlSmtproute.php
@@ -31,11 +31,66 @@ function doConfig(){
 	}
 	$handle=fopen("/var/qmail/control/smtproutes","w");	
 	if (!$handle) {
-		echo "错误，无法保存设置！<br>";
+		echo "错误，无法保存设置#1！<br>";
 		return false;
 	}
-	fputs($handle,str_replace("\r","",$_POST['content']));
+	$content = str_replace("\r","",$_POST['content']);
+	fputs($handle, $content);
 	fclose($handle);
+
+	$lines = split ( "\n", $content );
+
+	$route_domains = array();
+	foreach ( $lines as $line ){
+		list($domain, $ip, $port) = explode(":", $line);
+		//echo "$domain:$ip:$port<br>";
+		if ( strlen($domain) ){
+			array_push( $route_domains, $domain );
+		}
+	}
+
+	// modify rcpthosts
+	if ( ! file_exists("/var/qmail/control/rcpthosts") ){
+		fclose( fopen("/var/qmail/control/rcpthosts","w") );
+	}
+	
+	$handle=fopen("/var/qmail/control/rcpthosts","r");	
+	if (!$handle) {
+		echo "错误，无法保存设置#2！<br>";
+		return false;
+	}
+
+	//FIXME: only can add new domain, but never delete old domains.
+	$rcpt_domains = array();
+	while ( !feof($handle) ){
+		$domain = fgets ( $handle );
+		$domain = rtrim($domain);
+		if ( strlen($domain) ){
+			array_push( $rcpt_domains, $domain );
+		}
+	}
+	fclose($handle);
+
+	$add_route_domains = array_diff ( $route_domains, $rcpt_domains) ;
+
+	foreach ( $add_route_domains as $add_domain ){
+		array_push ( $rcpt_domains, $add_domain );
+	}
+
+	$rcpthost_content = "";
+	foreach ( $rcpt_domains as $domain ){
+		$rcpthost_content .= $domain ;
+		$rcpthost_content .= "\n" ;
+	}
+
+	$handle=fopen("/var/qmail/control/rcpthosts","w");	
+	if (!$handle) {
+		echo "错误，无法保存设置#2！<br>";
+		return false;
+	}
+	fputs($handle,$rcpthost_content);
+	fclose($handle);
+
 	return true;
 }
 
