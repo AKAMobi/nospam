394086b19b62b781ee659576ce857246d6180dab
Zhuohuan LI <zixia@zixia.net>
2004-03-07 14:34:11 +0000

    add dynamic engine 2 test

--- NoSPAM/Bin/case_test.pl
+++ NoSPAM/Bin/case_test.pl
@@ -4,9 +4,40 @@ use AKA::Mail::Spam;
 
 $S = new AKA::Mail::Spam;
 
-&test_spam_checker;
+&test_dynamic_from;
 #&test_traceable;
 ############################
+sub test_dynamic_from
+{
+	use AKA::Mail::Dynamic;
+	my $AMD = new AKA::Mail::Dynamic;
+
+	my $from = "hehe\@zixia.net";
+
+	if ( $AMD->is_overrun_rate_per_mailfrom( $from ) ){
+		print "from: $from is OVERRUN!\n";
+	}else{
+		print "from: $from is NOT overrun!\n";
+	}
+	$AMD->dump or die "can't dump";
+}
+
+
+sub test_dynamic_subject
+{
+	use AKA::Mail::Dynamic;
+	my $AMD = new AKA::Mail::Dynamic;
+
+	my $subject = "This is a subject - 2";
+
+	if ( $AMD->is_overrun_rate_per_subject( $subject ) ){
+		print "subject: $subject is OVERRUN!\n";
+	}else{
+		print "subject: $subject is NOT overrun!\n";
+	}
+	$AMD->dump or die "can't dump";
+}
+
 sub test_spam_checker
 {
 	$ip = "166.111.168.8";
