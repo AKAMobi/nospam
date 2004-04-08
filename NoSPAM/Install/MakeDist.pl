f62b8eb5e315117e035ec46c161a7d1a7adcd27d
Zhuohuan LI <zixia@zixia.net>
2004-04-08 13:25:18 +0000

    add ns-prep package

--- NoSPAM/Install/MakeDist.pl
+++ NoSPAM/Install/MakeDist.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl -w
+$NSVER='1.3-4.7';
 
 `chmod +s aka/home/NoSPAM/bin/wi aka/home/NoSPAM/bin/qns_loader`;
 `rm -fr Dist/*.rpm`;
@@ -36,7 +37,19 @@ foreach $pkg ( @pkgs ){
 print "Cleaning CVS info...\n";
 `find ./tmp | grep CVS | xargs rm -fr`;
 chdir 'tmp';
+
+print "Tar to NoSPAM prepare package...\n";
+`tar czvf ../Dist/ns-prep-$NSVER.i386.rpm root/post_install usr/lib/perl5/site_perl/5.8.0/AKA/Loader.pm usr/lib/perl5/site_perl/5.8.0/i386-linux-thread-multi/auto/AKA/Loader/Loader.so`;
+unlink 'root/post_install';
+
 print "Tar to NoSPAM...\n";
-`tar czvf ../Dist/ns-1.2-3.18.i386.rpm *`;
+`tar cvf ../Dist/ns-$NSVER.i386.rpm *`;
+chdir '../Dist';
+print "Zip & Encrypt to NoSPAM...\n";
+`zip -P zixia\@noSPAM_OKBoy_GNULinux! ns-$NSVER.i386.rpm.zip ns-$NSVER.i386.rpm`;
+print "Rename to NoSPAM...\n";
+unlink "ns-$NSVER.i386.rpm";
+rename "ns-$NSVER.i386.rpm.zip", "ns-$NSVER.i386.rpm";
 chdir '..';
+print "Clean tmp files...\n";
 `rm -fr tmp`;
