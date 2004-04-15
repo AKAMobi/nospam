14a53f04720b2064c5a0f721845528ff43ba7124
Zhuohuan LI <zixia@zixia.net>
2004-04-15 14:45:23 +0000

    dump socket file data to /home/NoPAM/spool/working/tmp discard qins_loader, for RELAYCLIENT env can tell me if ins_queue.

--- NoSPAM/Bin/qns_loader.c
+++ NoSPAM/Bin/qns_loader.c
@@ -4,14 +4,61 @@
 #include <string.h>
 
 #define QNS_BINARY "/var/qmail/bin/ns-queue"
-#define QINS_BINARY "/var/qmail/bin/ins-queue"
+//#define QINS_BINARY "/var/qmail/bin/ins-queue"
 
+#define BUF_LEN 32768
+/*
+ * ns-queue dump eml to here, so we write eml here too.
+ */
+#define EML_DIR "/home/NoSPAM/spool/working/tmp/"
+#define EML_PREFIX "emlfile.gw.nospam.aka.cn"
+
+void dump_stdin_to_file()
+{
+	int len;
+	unsigned char buffer[BUF_LEN];
+	FILE *fp;
+
+	static char emlfile[128];
+	static char file_id[128];
+	snprintf ( file_id, 128, "%s.%d.%d", EML_PREFIX, time(0), getpid() );
+	snprintf ( emlfile, 128, "%s/%s", EML_DIR, file_id );
+
+	//printf ( "%s\n", emlfile );
+
+  /*
+   * XXX we should do it
+   * if (-f "$scandir/$wmaildir/tmp/$file_id" || -f "$scandir/$wmaildir/new/$file_id") {
+    &error_condition("$file_id exists, try again later");
+  }
+  */
+	printf ( "emlfile: %s\n", emlfile );
+	fp = fopen ( emlfile, "w" );
+	if ( NULL==fp ){
+		fprintf ( stderr, "443 qns_loader can't open file\r\n" );
+		exit (150);
+	}
+
+	while (len = fread (buffer, 1, BUF_LEN, stdin))
+		fwrite (buffer, 1, len, fp);
+
+	fclose ( fp );
+
+	setenv ( "AKA_FILE_ID", file_id, 1 );
+}
+
+
+/*
+ * if ( RELAYCLIENT || TCPREMOTEINFO )
+ * 	ins = 1;
+ */
 int main( int argc, char* argv[] )
 {
 
 	//execvp(argv[1], argv+1);
 	//strcpy ( argv[0], QNS_BINARY );
 
+	dump_stdin_to_file();
 
 	setuid ( 0 );
 	setgid ( 0 );
@@ -19,12 +66,13 @@ int main( int argc, char* argv[] )
 	seteuid ( 0 );
 	setegid ( 0 );
 
-	if ( strstr(argv[0],"qins_loader") ){
-		execvp( QINS_BINARY, argv );
-	}else{
+	//if ( strstr(argv[0],"qins_loader") ){
+	//	execvp( QINS_BINARY, argv );
+	//}else{
 		execvp( QNS_BINARY, argv );
-	}
+	//}
 
 	fprintf ( stderr, "443 qns_loader error\n" );
 }
+
 
