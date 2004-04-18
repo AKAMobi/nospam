0d1d4e35bdefe3dca951df1087ef45fb418c0e20
Zhuohuan LI <zixia@zixia.net>
2004-04-18 22:25:53 +0000

    new client/server mode ns-queue client module

---
+++ NoSPAM/Lib/AKA/MailClient.pm
@@ -0,0 +1,56 @@
+#!/usr/bin/perl -w
+
+package AKA::MailClient;
+
+
+use IO::Socket();
+
+sub new
+{
+	my $class = shift;
+
+	my $self = {};
+
+	bless $self, $class;
+
+	my $parent = shift;
+
+	return $self;
+}
+
+sub net_process
+{
+	my $self = shift;
+	my $mail_info = shift;
+
+        my $socket = IO::Socket::INET->new(Proto =>"tcp",
+                                Timeout => 10,
+                                PeerAddr =>'127.0.0.1',
+                                PeerPort =>40307
+                                 ) ;
+
+        unless ( $socket && $socket->connected ){
+                return undef;
+        }
+
+	print $socket ($mail_info->{aka}->{RELAYCLIENT}?1:0) . "\n";
+	print $socket $mail_info->{aka}->{REMOTEIP} . "\n";
+	print $socket $mail_info->{aka}->{REMOTEINFO} . "\n";
+	print $socket $mail_info->{aka}->{emlfilename} . "\n";
+	print $socket $mail_info->{aka}->{fd1}  . "\n";
+
+	$mail_info->{aka}->{resp}->{smtp_code} = <$socket>;
+	chomp $mail_info->{aka}->{resp}->{smtp_code};
+
+	$mail_info->{aka}->{resp}->{smtp_info} = <$socket>;
+	chomp $mail_info->{aka}->{resp}->{smtp_info};
+
+	$mail_info->{aka}->{resp}->{exit_code} = <$socket>;
+	chomp $mail_info->{aka}->{resp}->{exit_code};
+	
+	close $socket;
+
+	$mail_info;
+}
+
+1;
