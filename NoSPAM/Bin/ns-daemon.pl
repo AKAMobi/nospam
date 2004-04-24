17c20fdba32197e0e4c312bff1e3dd90f4f04231
Zhuohuan LI <zixia@zixia.net>
2004-04-24 08:40:29 +0000

    add taint mode to ns-daemon

--- NoSPAM/Bin/ns-daemon.pl
+++ NoSPAM/Bin/ns-daemon.pl
@@ -1,12 +1,14 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl -Tw
 use AKA::Mail;
 
+$ENV{'PATH'} = '/bin:/sbin:/usr/bin:/usr/sbin';
+delete @ENV{'IFS', 'CDPATH', 'ENV', 'BASH_ENV'};
 
-close ( STDERR );
-open (STDERR, ">/var/log/NoSPAM.stderr");
+#close ( STDERR );
+#open (STDERR, ">/var/log/NoSPAM.stderr");
 
-close ( STDOUT );
-open (STDOUT, ">/var/log/NoSPAM.stdout");
+#close ( STDOUT );
+#open (STDOUT, ">/var/log/NoSPAM.stdout");
 
 my $AM = new AKA::Mail;
 
