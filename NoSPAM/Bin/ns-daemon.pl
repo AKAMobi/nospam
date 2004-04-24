2b3753c82886d676cc2b2c8d3e02507f38f61891
Zhuohuan LI <zixia@zixia.net>
2004-04-24 10:06:54 +0000

    redirect stderr

--- NoSPAM/Bin/ns-daemon.pl
+++ NoSPAM/Bin/ns-daemon.pl
@@ -1,14 +1,14 @@
-#!/usr/bin/perl -Tw
+#!/usr/bin/perl 
 use AKA::Mail;
 
 $ENV{'PATH'} = '/bin:/sbin:/usr/bin:/usr/sbin';
 delete @ENV{'IFS', 'CDPATH', 'ENV', 'BASH_ENV'};
 
-#close ( STDERR );
-#open (STDERR, ">/var/log/NoSPAM.stderr");
+close ( STDERR );
+open (STDERR, ">/var/log/NoSPAM.stderr");
 
-#close ( STDOUT );
-#open (STDOUT, ">/var/log/NoSPAM.stdout");
+close ( STDOUT );
+open (STDOUT, ">/var/log/NoSPAM.stdout");
 
 my $AM = new AKA::Mail;
 
