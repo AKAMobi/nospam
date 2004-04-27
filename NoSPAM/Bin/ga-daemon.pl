17a0f18c31137e4bf4e15ce2fe51def461451720
Zhuohuan LI <zixia@zixia.net>
2004-04-27 02:53:08 +0000

    增加公安网络协议总daemon

---
+++ NoSPAM/Bin/ga-daemon.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w
+use AKA::Mail::GA::GAISC;
+
+$ENV{'PATH'} = '/bin:/sbin:/usr/bin:/usr/sbin';
+delete @ENV{'IFS', 'CDPATH', 'ENV', 'BASH_ENV'};
+
+close ( STDERR );
+open (STDERR, ">>/var/log/NoSPAM.stderr");
+
+close ( STDOUT );
+open (STDOUT, ">>/var/log/NoSPAM.stdout");
+
+my $GA = new AKA::Mail::GA::GAISC;
+
+$GA->start_daemon_process;
+
