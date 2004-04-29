ab15e2b56a2d6563ae6f1f447793136756900e64
Zhuohuan LI <zixia@zixia.net>
2004-04-29 04:47:40 +0000

    支持25端口的SMTPS/SSL/TSL

--- NoSPAM/Install/root/post_proc.sh
+++ NoSPAM/Install/root/post_proc.sh
@@ -6,10 +6,13 @@ chmod g+s service/*/log
 chmod o+t service/*
 chmod o+t service/*/log
 
-chown qmaill service/*/log/{main,status}
+chown qmaill service/{smtpd,ismtpd,pop3d,qmail}/log/{main,status}
 chown nospam service/nsd/log/{main,status}
+chown nospam service/gad/log/{main,status}
 
 chmod +x service/*/run
 chmod +x etc/cron.hourly/*
 
 ln -s /home/NoSPAM/bin/NoSPAM etc/rc.d/rc3.d/S11NoSPAM_start_System
+ln -s /var/qmail/control/servercert.pem var/qmail/control/clientcert.pem
+chown qmaild.qmail var/qmail/control/*.pem
