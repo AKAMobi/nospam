3a8dc339eda636e47f12ce2aef9532c6032d54ae
Zhuohuan LI <zixia@zixia.net>
2004-05-07 13:48:34 +0000

    增加pop3s 增加noSPAM开机画面 使gad/nsd/pop3s写入multilog 使用supervise管理named

--- NoSPAM/Install/root/post_proc.sh
+++ NoSPAM/Install/root/post_proc.sh
@@ -7,8 +7,8 @@ chmod o+t service/*
 chmod o+t service/*/log
 
 chown qmaill service/{smtpd,ismtpd,pop3d,qmail}/log/{main,status}
-chown nospam service/nsd/log/{main,status}
-chown nospam service/gad/log/{main,status}
+chown nospam service/{nsd,gad,pop3s}/log/{main,status}
+#chown nospam service/gad/log/{main,status}
 chown named service/named/log/main
 
 chmod +x service/*/run
@@ -18,3 +18,4 @@ chmod +x etc/cron.hourly/*
 ln -s /home/NoSPAM/bin/NoSPAM etc/rc.d/rc3.d/S11NoSPAM_start_System
 ln -s /var/qmail/control/servercert.pem var/qmail/control/clientcert.pem
 chown qmaild.qmail var/qmail/control/*.pem
+chmod 700 /var/qmail/control/*.pem
