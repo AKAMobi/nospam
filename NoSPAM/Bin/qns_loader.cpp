ef71eb8f7dfc6c1be838b2c4ff3a4c6feaf44da3
Zhuohuan LI <zixia@zixia.net>
2004-05-21 17:58:12 +0000

    C++中间产物

--- NoSPAM/Bin/qns_loader.cpp
+++ NoSPAM/Bin/qns_loader.cpp
@@ -4,15 +4,14 @@
  * 2004-05-21
  */
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <time.h>
 
+#include <string>
 
 #include <socket++/sockinet.h>
 
@@ -20,13 +19,47 @@
 #include <boost/filesystem/fstream.hpp>    // ditto
 #include <boost/lexical_cast.hpp>
 
-using namespace boost::filesystem;
 //using namespace boost::lexical_cast;
+using namespace boost::filesystem;
 
 
-#define BUF_LEN 32768
-#define EML_DIR "/home/NoSPAM/spool/working/"
-#define EML_PREFIX "emlfile.gw.nospam.aka.cn"
+#define BUF_LEN 4096
+#define EML_DIR "/home/NoSPAM/spool/working"
+#define EML_PREFIX "emlfile.gw.nospam"
+
+class qmail_hdrs
+{
+	public:
+		char invalid_hdrs[6] ;
+		size_t invalid_len ;
+
+		char hdrs[2048];
+		size_t len;
+
+		qmail_hdrs( char *p, size_t n=0)
+		{
+			len = n;
+			for ( size_t i=0; i<n; i++ ){
+				hdrs[i] = p[n];
+			}
+
+			invalid_len = 5;
+			memcpy ( invalid_hdrs, "F\0T\0\0", invalid_len ) ;
+		}
+
+		~qmail_hdrs(){;}
+
+		bool valid()
+		{
+			if ( 0==len ) return false;
+
+			for ( size_t i=0; i<invalid_len; i++ )
+				if ( invalid_hdrs[i]!=hdrs[i] ) return true;
+
+			return false;
+		}
+
+};
 
 string dump_stdin_to_file()
 {
@@ -37,6 +70,7 @@ string dump_stdin_to_file()
 	char tmp_emlfile[128];
 	char new_emlfile[128];
 	char file_id[128];
+
 	snprintf ( file_id, 128, "%s.%d.%d", EML_PREFIX, (int)time(0), getpid() );
 	snprintf ( tmp_emlfile, 128, "%s/tmp/%s", EML_DIR, file_id );
 	snprintf ( new_emlfile, 128, "%s/new/%s", EML_DIR, file_id );
@@ -78,7 +112,7 @@ string dump_stdin_to_file()
 
 int net_process( string &result, 
 		string relayclient, string tcpremoteip, 
-		string tcpremoteinfo, string emlfile, string hdrs )
+		string tcpremoteinfo, string emlfile, qmail_hdrs *hdrs )
 {
 	iosockinet io (sockbuf::sock_stream);
 
@@ -89,23 +123,28 @@ int net_process( string &result,
 		return -1;
 	}
 	
+cout << relayclient << "," << tcpremoteip << "," << tcpremoteinfo << "," << emlfile << "," << hdrs->len << endl;
 	io << relayclient << endl;
 	io << tcpremoteip << endl;
 	io << tcpremoteinfo << endl;
 	io << emlfile << endl;
-	io << hdrs << endl;
+	//io << hdrs << endl;
+	io<<"ft"<<endl;
 
-	string smtp_code, smtp_info, exit_code;
-	io >> smtp_code;
-	io >> smtp_info;
-	io >> exit_code;
+	char smtp_code[40+1], smtp_info[256+1], exit_code[4+1];
+	io.getline( smtp_code, 40 );
+	io.getline( smtp_info, 256 );
+	io.getline( exit_code, 4 );
 
-	result = smtp_code + " " + smtp_info;
+cout << "smtp_code: [" << smtp_code << "] smtp_info: [" << smtp_info << "] exit_code [" << exit_code << "]" << endl;
+	result = smtp_code;
+	result += " ";
+        result += smtp_info;
 
 	int ret;
 	try {
 		//ret = lexical_cast<int>exit_code;
-		ret = atoi(exit_code.c_str());
+		ret = atoi(exit_code);
 	} catch (...) {
 		ret = -1;
 	}
@@ -114,16 +153,15 @@ int net_process( string &result,
 	return ret;
 }
 
-string grab_envelope_hdrs()
+qmail_hdrs* grab_envelope_hdrs()
 {
-	string hdrs;
-	char buf[80+1];
-	ssize_t n;
-	while ( 0<(n=read(1, buf, 80 )) )
-	{
-		buf[n] = 0;
-		hdrs += buf;
-	}
+	char buf[2048];
+	size_t n;
+
+	n = read( 1, buf, sizeof(buf) );
+	qmail_hdrs *hdrs = new qmail_hdrs ( buf, n );
+	//if ( -1==n )
+
 	return hdrs;
 }
 
@@ -169,11 +207,18 @@ int old_main( int argc, char* argv[] )
 
 int main ()
 {
+
 	string relayclient,tcpremoteinfo,tcpremoteip;
 
 	char *RELAYCLIENT = getenv("RELAYCLIENT");
 	char *TCPREMOTEINFO = getenv("TCPREMOTEINFO");
 	char *TCPREMOTEIP = getenv("TCPREMOTEIP");
+
+	string emlfile;
+	qmail_hdrs *hdrs;
+
+	int ret;
+	string result;
 
 	if ( RELAYCLIENT && strlen(RELAYCLIENT) )
 		//relayclient = RELAYCLIENT;
@@ -183,34 +228,42 @@ int main ()
 	if ( TCPREMOTEIP && strlen (TCPREMOTEIP) )
 		tcpremoteip = TCPREMOTEIP;
 
-	//cout << tcpremoteinfo << "," << relayclient << endl;
+	//goto LABEL;
+	cout << tcpremoteinfo << "," << relayclient << endl;
 
-	string emlfile = dump_stdin_to_file();
+	emlfile = dump_stdin_to_file();
 	//cout << "file_id: " << file_id << endl;
 	
 	if ( !exists( emlfile ) ){
 		qns_err("443 ns can't get file.", 150);
 	}
 
-	string hdrs = grab_envelope_hdrs();
-	if ( !hdrs.length() || "F\0T\0\0"==hdrs )
-	{
+	hdrs = grab_envelope_hdrs();
+	if ( !hdrs->valid() ) {
 	  debug("g_e_h: no sender and no recips.");
 	  //unlink(emlfile.c_str())
 	  remove(emlfile);
 	  qns_err("443 ns can't get hdr.", 150);
 	}
-
+for ( size_t i=0; i<hdrs->len; i++ ){
+	if ( 0==hdrs->hdrs[i] ){
+		cout << "\\0";
+	}else{
+		cout << hdrs->hdrs[i];
+	}
+}
+cout << endl;
 	qmail_parent_check();
-	int ret;
-	string result;
+LABEL:
 	ret = net_process( result, relayclient, tcpremoteip, tcpremoteinfo, emlfile, hdrs );
+	//ret = net_process( result, "127.0.0.2", "192.168.0.1", "", "/tmp/php.err", "Fzixia@zixia.net\0Tzixia@vmware.zixia.net\0\0");
 
 	if ( -1==ret ){
+		cerr << result << "\r\n";
 		exit (111);
 	}
 
-	cerr << result << "\r\n";
+	cerr << "[" << result << "]\r\n";
 	exit ( ret );
 }
 
