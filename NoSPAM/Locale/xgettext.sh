b9d4b99636a79aebdb7e0222e69fd07df4730a5c
Zhuohuan LI <zixia@zixia.net>
2004-06-12 12:53:46 +0000

    可以更新了

--- NoSPAM/Locale/xgettext.sh
+++ NoSPAM/Locale/xgettext.sh
@@ -2,12 +2,13 @@
 
 	#--add-comments=TRANSLATORS: --files-from=./POTFILES.in \
 
-xgettext --output=./engine.pox \
+xgettext --output=./engine.pot \
 	--copyright-holder="Imperia AG Huerth/Germany" \
 	--from-code=GBK \
 	--keyword --keyword='$__' --keyword=__ --keyword=__x \
 	--keyword=__n:1,2 --keyword=__nx:1,2 --keyword=__xn \
 	--keyword=N__ --language=perl $@ && \
-	rm -f engine.pot && \
-	mv engine.pox engine.pot
+	msgmerge engine.po engine.pot -o engine.pox && \
+	rm -f engine.pot engine.po && \
+	mv engine.pox engine.po
 
