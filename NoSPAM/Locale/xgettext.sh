567e1ce696714c41db64a0b1f33ae69129851bce
Zhuohuan LI <zixia@zixia.net>
2004-06-13 06:38:08 +0000

    add makefile

--- NoSPAM/Locale/xgettext.sh
+++ NoSPAM/Locale/xgettext.sh
@@ -2,13 +2,20 @@
 
 	#--add-comments=TRANSLATORS: --files-from=./POTFILES.in \
 
-xgettext --output=./engine.pot \
+xgettext --output=./engine.nospam.cn.pot \
 	--copyright-holder="Imperia AG Huerth/Germany" \
 	--from-code=GBK \
 	--keyword --keyword='$__' --keyword=__ --keyword=__x \
 	--keyword=__n:1,2 --keyword=__nx:1,2 --keyword=__xn \
 	--keyword=N__ --language=perl $@ && \
-	msgmerge engine.po engine.pot -o engine.pox && \
-	rm -f engine.pot engine.po && \
-	mv engine.pox engine.po
+	\
+	msgmerge engine.nospam.cn.po engine.nospam.cn.pot -o engine.nospam.cn.pox && \
+	\
+	rm -f engine.nospam.cn.pot engine.nospam.cn.po && \
+	mv engine.nospam.cn.pox engine.nospam.cn.po &&
+	\
+	msgfmt engine.nospam.cn.po -o engine.nospam.cn.mo && \
+	\
+	mv -f engine.nospam.cn.mo /home/NoSPAM/LocaleData/zh_CN/LC_MESSAGES 
 
+#iconv -f utf8 -t GBK engine.nospam.cn.pot 
