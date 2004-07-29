ce67e84c0bbbe3b6eb3adb03c629167f78f36204
Zhuohuan LI <zixia@zixia.net>
2004-07-29 17:55:59 +0000

    add User module

---
+++ NoSPAM/Lib/AKA/Mail/User.pm
@@ -0,0 +1,39 @@
+#
+# noSPAM 用户列表接口
+# Company: AKA Information & Technology Co., Ltd.
+# Author: Ed Li
+# EMail: zixia@zixia.net
+# Date: 2004-07-29
+
+package AKA::Mail::User;
+
+use strict;
+
+use AKA::Mail::DB;
+
+sub new
+{
+	my $class = shift;
+	my $self = {};
+	bless $self, $class;
+
+	my $parent = shift;
+
+	$self->{parent} = $parent;
+	$self->{zlog} = $parent->{zlog};
+	$self->{conf} = $parent->{conf};
+	
+	$self->{db} = new AKA::Mail::DB;
+
+	return $self;
+}
+
+sub is_user_exist($$)
+{
+	my $self = shift;
+	my $email = shift;
+	
+	return $self->{db}->user_email_exist( $email );
+}
+
+1;
