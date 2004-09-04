3b3e52cfbc47b9e52ffe94fbb514e147455bf7f9
Zhuohuan LI <zixia@zixia.net>
2004-09-04 04:47:39 +0000

    add user white list db

--- NoSPAM/Lib/AKA/Mail/DB.pm
+++ NoSPAM/Lib/AKA/Mail/DB.pm
@@ -58,12 +58,24 @@ sub create_table($$)
 
 	my $drop_sql = <<__SQL__;
 drop table UserEmail_TB;
+drop table UserWhiteList_TB;
 __SQL__
 
 	my $create_sql = <<__SQL__;
 create table UserEmail_TB (
 	Email varchar(64) primary key
 );
+create table UserWhiteList_TB ( 
+        AutoID INTEGER PRIMARY KEY, 
+        User VARCHAR(50) NOT NULL, 
+        Domain VARCHAR(50) NOT NULL, 
+        Email VARCHAR(80) NOT NULL, 
+        Type INT(1) NOT NULL DEFAULT '1' 
+);
+
+create index UWL_Email_IDX ON UserWhiteList_TB(Email); 
+create index UWL_User_IDX ON UserWhiteList_TB(User,Domain,Type); 
+
 __SQL__
 
 	my $dbh = $self->connect;
@@ -145,6 +157,44 @@ sub user_email_exist($$)
 	my ($count) = $sth->fetchrow_array();
 	$sth->finish;
 	return $count;
+}
+
+# 检查是否发件人被收件人列入白名单
+# 如果有多个收件人，则任何一个收件人将发件人列入白名单即可
+# 如果是白名单，返回1，否则返回0；
+#	参数：
+#		type: AKA::Mail::Conf::WHITE_LIST or BLACK_LIST，需要和数据库端一致
+
+sub is_user_whitelist
+{
+	my $self = shift;
+	my ($type,$sender,@receivers) = @_;
+
+	my $dbh = $self->connect();
+
+	# Whiteist Type = 1
+	# Blacklist Type = 2
+	my $sth = $dbh->prepare ( "select count(*) from UserWhiteList_TB where User=? and Domain=? and Email=? and Type=$type" );
+
+	my $count = 0;
+	my ($user,$domain) = ();
+	foreach my $receiver ( @receivers ){
+		($user,$domain) = split('@',$receiver);
+		$sth->execute( $user,$domain,$sender );
+		($count) = $sth->fetchrow_array();
+	
+		# in white list;
+		if ( $count > 0 ){
+			$sth->finish;
+			$self->{zlog}->debug( "DB::is_user_whitelist $user\@$domain treate $sender as [$type] list." );
+			return 1;
+		}
+	}
+
+	# not in white list
+	$sth->finish;
+	$self->{zlog}->debug( "DB::is_user_whitelist $user\@$domain NOT treate $sender as [$type] list." );
+	return 0;
 }
 
 1;
